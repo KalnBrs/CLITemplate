0e3a09c290bad46f37d7e9b0309e152f621c8aa3
Kaelan Brose <kbrose2776@gmail.com>
2025-11-25 20:17:32 -0600

    Implemented add to templates

--- CLIProjectTool/Commands/TemplateCommand.cs
+++ CLIProjectTool/Commands/TemplateCommand.cs
@@ -6,6 +6,8 @@ namespace CLIProjectTool.Commands
 {
     public class TemplateCommand : ICommand
     {
+        static readonly string destinationFolderPath = Path.Combine(Environment.CurrentDirectory, "Templates");
+
         public static Command Build()
         {
             var templateCommand = new Command("templates", "using the templates");
@@ -17,8 +19,6 @@ namespace CLIProjectTool.Commands
                 name,
                 path
             };
-
-            
 
             var removeCommand = new Command("remove", "remove a template")
             {
@@ -35,7 +35,31 @@ namespace CLIProjectTool.Commands
 
         private static void HandleAdd(string name, string path)
         {
-            Console.WriteLine($"This adds a tempate with this name {name}, and this path {path}");
+            string desFilePath = Path.Combine(destinationFolderPath, name + Path.GetExtension(path));
+            Console.WriteLine(destinationFolderPath);
+            Console.WriteLine($"This adds a tempate with this name {desFilePath}, and this path {path}");
+
+            try 
+            {
+                if (!Directory.Exists(destinationFolderPath))
+                {
+                    Directory.CreateDirectory(destinationFolderPath);
+                    Console.WriteLine($"Created directory: {destinationFolderPath}");
+                }
+
+                if (!File.Exists(desFilePath))
+                {
+                    using (File.Create(desFilePath))
+                    {
+                    }
+                }
+
+                File.Copy(path, desFilePath, true);
+            }
+            catch (Exception err)
+            {
+                Console.Error.WriteLine(err.Message);
+            }
         }
 
         private static void HandleRemove(string name)
